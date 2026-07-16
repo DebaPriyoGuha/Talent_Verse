05cba1ec2cad079abaf92d459747c256c02b3bbe
DebaPriyoGuha <90377346+DebaPriyoGuha@users.noreply.github.com>
2026-07-16 23:45:13 +0000

    v10: featured top3 + all-category mix; buildPostCard try/catch; better rules error msg

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-import { auth, database } from './firebase-config.js?v=9';
+import { auth, database } from './firebase-config.js?v=10';
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
