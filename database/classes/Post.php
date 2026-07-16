147c38372f48280b91fd26f122efcc76d1de438b
DebaPriyoGuha <90377346+DebaPriyoGuha@users.noreply.github.com>
2026-07-16 19:27:57 +0000

    Post.php: add tags support to submitPost() and indexPosts()

--- database/classes/Post.php
+++ database/classes/Post.php
@@ -1,184 +1,125 @@
-<?php 
-
-    class Post{
-        private $user_obj;
-        private $con;
-        
-        public function __construct($con, $user){
-            $this->con = $con;
-            $this->user_obj = new User($con, $user);
-        }
-        
-        public function submitPost($body, $imageName){
-            $body = strip_tags($body); //remove thigs like <,>...etc tages
-            $body = mysqli_real_escape_string($this->con, $body); //egnore the ' in post boddy
-            $check_empty = preg_replace('/\s+/', '', $body); //deletes all spaces
-            
-            if($check_empty != ""){
-                $body_array = preg_split("/\s+/", $body);
-                $body = implode(" ", $body_array);
-                
-                //curentdate and time
-                $date_added = date("Y-m-d H:i:s");
-                
-                //get username 
-                $added_by = $this->user_obj->getUsername();
-                
-                //insert post to database
-                $query = mysqli_query($this->con, "INSERT INTO posts (body, added_by, date_added, user_closed, deleted, likes, image) VALUES('$body', '$added_by', '$date_added', 'no', 'no', '0', '$imageName')");
-
-                //returns the id of iserted post
-                $returned_id = mysqli_insert_id($this->con);
-                
-                //incereas the post no of usr 
-                $num_posts = $this->user_obj->getNumPosts();
-                $num_posts++;
-                $update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
-            }
-        }
-
-        public function indexPosts () {
-          
-            $ret_str = "";
-            $data_query = mysqli_query($this->con, "SELECT * FROM posts ORDER BY id DESC");
-    
-                while($row = mysqli_fetch_array($data_query)) {
-                    $id = $row['id'];
-                    $body = $row['body'];
-                    $added_by = $row['added_by'];
-                    $date_time = $row['date_added'];
-                    $imagePath = $row['image'];
-
-                    // show post only from the friends 
-                    // $userLoggedIn = $_SESSION['username'];
-                    // $user_logged_obj = new User($this->con, $userLoggedIn);
-				    // if($user_logged_obj->isFriend($added_by)){
-
-                        // show post/display post
-                        $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
-                        $user_row = mysqli_fetch_array($user_details_query);
-                        $first_name = $user_row['first_name'];
-                        $last_name = $user_row['last_name'];
-                        $profile_pic = $user_row['profile_pic'];
-                        
-                        ?>
-
-<script>
-function toggle<?php echo $id; ?>() {
-
-
-    var element = document.getElementById("toggleComment<?php echo $id; ?>");
-
-    if (element.style.display == "block")
-        element.style.display = "none";
-    else
-        element.style.display = "block";
-
-}
-</script>
-
 <?php
-                        // count comments
-                        $comment_check = mysqli_query($this->con,"select * from comments where post_id='$id'");
-                        $comment_check_num = mysqli_num_rows($comment_check);
 
-                        $date_time_now = date("Y-m-d H:i:s");
-                        $start_date = new DateTime($date_time); //time of post
-                        $end_date = new DateTime($date_time_now); //curent time
-                        $interval = $start_date->diff($end_date); //difrent between dates
-                        
-                        if($interval->y >= 1){
-                            if($interval->y == 1)
-                                $time_message = $interval->y . " year ago";
-                            else
-                                $time_message = $interval->y . " years ago";
-                        }
-                        else if($interval->m >= 1){
-                            if($interval->d == 0){
-                                $days = " ago";
-                            }
-                            else if($interval->d == 1){
-                                $days = $interval->d . " day ago";
-                            }
-                            else{
-                                $days = $interval->d . " days ago";
-                            }
-                            
-                            if($interval->m == 1){
-                                $time_message = $interval->m . " month" .
-                                $days;
-                            }
-                            else{
-                                $time_message = $interval ->m . " months".
-                                $days;
-                            }
-                        }
-                        
-                        else if($interval->d >= 1){
-                            if($interval->d == 1){
-                                $time_message = "Yesterday";
-                            }
-                            else{
-                                $time_message = $interval->d . " days ago";
-                            }
-                        }
-                        
-                        else if($interval->h >= 1){
-                            if($interval->h == 1){
-                                $time_message = $interval->h . " hour ago";
-                            }
-                            else{
-                                $time_message = $interval->h . " hours ago";
-                            }
-                        }
-                        
-                        else if($interval->i >= 1){
-                            if($interval->i == 1){
-                                $time_message = $interval->i . " minute ago";
-                            }
-                            else{
-                                $time_message = $interval->i . " minutes ago";
-                            }
-                        }
-                        
-                        else{
-                            if($interval->s < 30){
-                                $time_message = "Just Now";
-                            }
-                            else{
-                                $time_message = $interval->s . " seconds ago";
-                            }
-                        }
-                        
-                        
-                        $ret_str .= "
-                            <div class='status_post'>                     
-                                <div class='post_profile_pic'>
-                                    <img src='$profile_pic' width='50' style='border-radius: 50%;'> 
-                                </div>  
-                                <div class='posted_by' style='color:#ACACAC;'> 
-                                    <a href='$added_by'> $first_name $last_name </a> <br> 
-                                    <div class='time'> $time_message </div> 
-                                </div> <br> <br> 
-                                <div class='post_body' id='post_body'> 
-                                <span style='margin-left: 34px;'> $body </span> <br> <br> <img src='$imagePath'> <br> 
-                                </div> 
-                            </div>
-                            <div class='post_feature'>
-                                <span class='comment' style='color: #3875c5; font-size: 12px; float: right; margin-right: 40px; margin-top:-10px;'  onClick='javascript:toggle$id()'><i class='fas fa-comment fa-2x'></i>($comment_check_num)</span>&nbsp;&nbsp;
-                                <iframe src='like.php?post_id=$id'style='border: 0px; height: 30px; width: 120px; margin-left: 35px;' scrolling='no'></iframe>
-                            </div>
-                            <div class='post_comment' id='toggleComment$id' style='display:none;'>
-                                <iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0' style='display: flex; width: 100%; border-radius: 5px;'></iframe>                                    
-                            </div>
-                            <hr style='margin-bottom: 28px;'> ";
-                    // }//end if              
-                }//end of loop
-                
-            echo $ret_str;
-        }//end indexpost
+class Post {
+    private $user_obj;
+    private $con;
 
-       
+    public function __construct($con, $user) {
+        $this->con      = $con;
+        $this->user_obj = new User($con, $user);
+    }
 
-    }//end class
+    public function submitPost($body, $imageName, $tag = 'other') {
+        $body = strip_tags($body);
+        $body = mysqli_real_escape_string($this->con, $body);
+        $tag  = mysqli_real_escape_string($this->con, $tag);
+        $body = implode(' ', preg_split('/\s+/', $body));
+
+        if (trim($body) === '' && $imageName === '') return;
+
+        $date_added = date('Y-m-d H:i:s');
+        $added_by   = $this->user_obj->getUsername();
+
+        mysqli_query($this->con,
+            "INSERT INTO posts (body, added_by, date_added, user_closed, deleted, likes, image, tags)
+             VALUES('$body', '$added_by', '$date_added', 'no', 'no', '0', '$imageName', '$tag')"
+        );
+
+        $id          = mysqli_insert_id($this->con);
+        $num_posts   = $this->user_obj->getNumPosts() + 1;
+        mysqli_query($this->con,
+            "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'"
+        );
+        return $id;
+    }
+
+    public function indexPosts($filter = 'all', $categories = []) {
+        $where = "WHERE p.deleted='no' AND p.user_closed='no'";
+        if ($filter !== 'all') {
+            $f     = mysqli_real_escape_string($this->con, $filter);
+            $where .= " AND p.tags='$f'";
+        }
+
+        $query = mysqli_query($this->con,
+            "SELECT p.*, u.first_name, u.last_name, u.profile_pic
+             FROM posts p
+             JOIN users u ON u.username = p.added_by
+             $where
+             ORDER BY p.id DESC"
+        );
+
+        if (mysqli_num_rows($query) === 0) {
+            echo "<div style='text-align:center;padding:40px;color:#6b7280;'>
+                    <i class='fas fa-star fa-2x' style='margin-bottom:12px;color:#c4b5fd'></i><br>
+                    No posts here yet. Be the first to share your talent!
+                  </div>";
+            return;
+        }
+
+        $now = new DateTime();
+
+        while ($row = mysqli_fetch_array($query)) {
+            $id          = (int) $row['id'];
+            $body        = htmlspecialchars($row['body']);
+            $added_by    = $row['added_by'];
+            $first_name  = htmlspecialchars($row['first_name']);
+            $last_name   = htmlspecialchars($row['last_name']);
+            $profile_pic = htmlspecialchars($row['profile_pic'] ?: 'assets/images/default.png');
+            $imagePath   = htmlspecialchars($row['image']);
+            $tag         = $row['tags'] ?? 'other';
+            $tag_class   = isset($categories[$tag]) ? $categories[$tag]['class'] : 'tag-other';
+            $tag_label   = isset($categories[$tag]) ? $categories[$tag]['label'] : ucfirst($tag);
+            $tag_icon    = isset($categories[$tag]) ? $categories[$tag]['icon']  : 'fas fa-star';
+
+            // Time ago
+            $post_time = new DateTime($row['date_added']);
+            $diff      = $now->diff($post_time);
+            if ($diff->y >= 1)      $time_msg = $diff->y . 'y ago';
+            elseif ($diff->m >= 1)  $time_msg = $diff->m . 'mo ago';
+            elseif ($diff->d >= 1)  $time_msg = $diff->d . 'd ago';
+            elseif ($diff->h >= 1)  $time_msg = $diff->h . 'h ago';
+            elseif ($diff->i >= 1)  $time_msg = $diff->i . 'min ago';
+            else                    $time_msg = 'Just now';
+
+            // Comment count
+            $c_q   = mysqli_query($this->con, "SELECT COUNT(*) AS c FROM comments WHERE post_id='$id'");
+            $c_row = mysqli_fetch_array($c_q);
+            $c_cnt = (int) $c_row['c'];
+
+            // Like count
+            $likes = (int)($row['likes'] ?? 0);
+
+            $img_html = '';
+            if ($imagePath) {
+                $img_html = "<img src='$imagePath' class='post-image' alt='post image'>";
+            }
+
+            echo "
+            <div class='post-card' id='post_$id'>
+                <div class='post-header'>
+                    <img src='$profile_pic' class='post-avatar' alt=''>
+                    <div class='post-meta'>
+                        <div class='post-author'><a href='profile.php?profile_username=" . htmlspecialchars($added_by) . "'>$first_name $last_name</a></div>
+                        <div class='post-time'>$time_msg</div>
+                    </div>
+                    <a href='index.php?tag=$tag' class='post-tag-badge $tag_class'>
+                        <i class='$tag_icon'></i> $tag_label
+                    </a>
+                </div>
+                " . ($body ? "<div class='post-body'>$body</div>" : '') . "
+                $img_html
+                <div class='post-footer'>
+                    <iframe src='like.php?post_id=$id' style='border:0;height:30px;width:110px' scrolling='no'></iframe>
+                    <button class='post-action' onclick='toggleComments($id)'>
+                        <i class='fas fa-comment'></i> $c_cnt
+                    </button>
+                </div>
+                <div class='post-comments' id='comments_$id'>
+                    <iframe src='comment_frame.php?post_id=$id' frameborder='0'
+                            style='width:100%;min-height:180px;display:block'></iframe>
+                </div>
+            </div>";
+        }
+    }
+}
 ?>
