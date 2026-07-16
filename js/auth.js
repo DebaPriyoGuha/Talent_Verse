f9429d3dc2b112b9bfe69c348e0341dce56f6356
DebaPriyoGuha <90377346+DebaPriyoGuha@users.noreply.github.com>
2026-07-16 23:32:18 +0000

    v=8 cache-bust

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-import { auth, database } from './firebase-config.js?v=7';
+import { auth, database } from './firebase-config.js?v=8';
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
